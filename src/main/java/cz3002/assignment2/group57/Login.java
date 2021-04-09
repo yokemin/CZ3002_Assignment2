ffbe878950c3b5166d7da6def7888f9d18e857b0
yokemin <woonyokemin@gmail.com>
2021-04-09 23:48:09 +0800

    Remove commented code

--- src/main/java/cz3002/assignment2/group57/Login.java
+++ src/main/java/cz3002/assignment2/group57/Login.java
@@ -8,55 +8,6 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class Login extends ActionSupport implements SessionAware {
 
-//	private Account account;
-//	SessionMap<String, String> sessionmap;
-//
-//	public String usernameRequired = "Username is required.";
-//    public String passwordRequired = "Password is required.";
-//
-//	public String getUsername() {  
-//	    return account.getUsername();  
-//	}
-//
-//	public String getPassword() {
-//		return account.getPassword();
-//	}
-//
-//	public Account getAccount() {
-//		return account;
-//	}
-//
-//	public void setAccount(String username, String password) {  
-//	    this.account.setUsername(username);  
-//		this.account.setPassword(password);  
-//	}
-//
-//	public String execute() {
-//		if (getUsername().length() == 0) {
-//            addFieldError("username", usernameRequired);
-//		}
-//
-//		if (getPassword().length() == 0) {
-//            addFieldError("password", getText(passwordRequired));
-//        }
-//
-//		if (LoginDAO.validate(getUsername(), getPassword())) {  
-//	        return "success";  
-//	    } else {  
-//	        return "error";  
-//	    }  
-//	}  
-//
-//	public void setSession(Map map) {  
-//	    sessionmap = (SessionMap)map;  
-//	    sessionmap.put("login", "true");  
-//	}
-//
-//	public String logout() {  
-//	    sessionmap.invalidate();  
-//	    return "success";  
-//	}  
-
 	private String user, pass;
 	private Account account;
 	SessionMap<String, String> sessionmap;
