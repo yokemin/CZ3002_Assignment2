db846a882e13e84776ff3872ae12b85e43b27c82
codergan <ganx0005@e.ntu.edu.sg>
2021-04-09 12:30:14 +0800

    use Account model

---
+++ src/main/java/cz3002/assignment2/group57/Account.java
@@ -0,0 +1,29 @@
+package cz3002.assignment2.group57;
+
+import java.util.Map;  
+
+  
+public class Account {  
+	
+	private String username, password;  
+	
+	public String usernameRequired = "Username is required.";
+    public String passwordRequired = "Password is required.";
+  
+	public String getUsername() {  
+	    return username;  
+	}  
+	  
+	public void setUsername(String username) {  
+	    this.username = username;  
+	}  
+	  
+	public String getPassword() {  
+	    return password;  
+	}  
+	  
+	public void setPassword(String password) {  
+	    this.password = password;  
+	} 
+	  
+}  
