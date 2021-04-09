a8108753102fa4a5c4227e7d47db1519d48e5b27
codergan <ganx0005@e.ntu.edu.sg>
2021-04-09 19:16:55 +0800

    verify account

--- src/main/java/cz3002/assignment2/group57/Account.java
+++ src/main/java/cz3002/assignment2/group57/Account.java
@@ -7,6 +7,16 @@ public class Account {
 	
 	private String username, password;  
   
+	public Account(String username, String password){
+		this.username = username;  
+		this.password = password;  
+   }
+
+   public Account(){
+	   this.username = "default";  
+	   this.password = "default";  
+   }
+
 	public String getUsername() {  
 	    return username;  
 	}  
---
+++ src/main/java/cz3002/assignment2/group57/AccountTest.java
@@ -0,0 +1,9 @@
+package cz3002.assignment2.group57;
+
+public class AccountTest{
+   public static void main(String[] args){
+	Account account  = new Account();
+	account.setUsername("Ethan");
+	System.out.println(account.getUsername());
+   }
+}
